bce00cdf8b267a20f9c4faa86554a9675056b4ac
Musa Jibril <musjib999@gmail.com>
2020-12-16 16:57:15 +0100

    git one and get all

--- index.js
+++ index.js
@@ -1,52 +1,81 @@
-const express = require("express");
-const bodyParser = require("body-parser");
+const express = require('express');
+const bodyParser = require('body-parser');
 const swaggerJSDOC = require('swagger-jsdoc');
 const swaggerUI = require('swagger-ui-express');
-const app = express();
 
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({extended:true}));
+const server = express();
+server.use(bodyParser.json());
+server.use(bodyParser.urlencoded({ extended: true }));
 
 const swaggerOptions = {
     definition: {
         info: {
             title: 'Scheduler RESTful API',
-            description: 'An API to Create or Schedule resource',
+            description: 'An API to Create schedules',
             version: '1.0',
             contact: {
-                name: 'LexClass Members',
-                email: 'almembers@lexclass.com',
-                ur: 'lexclass.com'
+                name: 'shamskhalil',
+                email: 'shamskhalil@gmail.com'
             }
         }
     },
-    apis: ['./src/restapi.js', './src/routes/*.js']
+    apis: ['./index.js']
 }
 const swaggerDef = swaggerJSDOC(swaggerOptions);
 
-app.use('/docs', swaggerUI.serve, swaggerUI.setup(swaggerDef));
+server.use('/docs', swaggerUI.serve, swaggerUI.setup(swaggerDef));
 
 /**
- * @swagger
- * /:
- *  get:
- *      description: "This is the root endpoint of our api"
- *      tags:
- *          - Root Endpoint
- *      responses:
- *          '200':
- *              description: 'Request is successful'
- *          '500':
- *              description: 'Request Failed'
- */
-app.post('/scheduler/add',(req,res)=>{
-    res.status(200).json({"status":"Ok"});
+     * @swagger
+     * /schedulerjobs/{id}:
+     *  get:
+     *      description: "Fetches a single schedule"
+     *      tags:
+     *          - get single schedule
+     *      parameters:
+     *          - name: id
+     *            type: string
+     *            description: 'The id of the schedule'
+     *            in: path
+     *            required: true
+     *          
+    
+     *      responses:
+     *          '200':
+     *              description: 'Request is successful'
+     *          
+     *          '500':
+     *              description: 'Request Failed'
+     */
+server.get('/schedulerjobs/:id',(req,res)=>{
+    res.status(200).json({"status":"single schedule"});
 });
 
-app.post('/scheduler/cancel/:id',(req,res)=>{
-    res.status(200).json({"status":"cancel schedule Sucess","payload":{}});
+
+/**
+     * @swagger
+     * /scheduler/active/:
+     *  get:
+     *      description: "Gets all active schedules "
+     *      tags:
+     *          - get all schedule
+     *      responses:
+     *          '200':
+     *              description: 'Request is successful'
+     *          '500':
+     *              description: 'Request Failed'
+     */
+
+
+
+server.get('/scheduler/active', (req, res) => {
+    res.status(200).json({OK: true})
+    console.log(JSON.stringify('All active schedules fetched'));
 });
 
-app.listen( 3000, ()=>{
-    console.log("App listening on port 3000");
+
+server.listen(3000, () => {
+    console.log('Server listening on port 3000');
 });
+
+
